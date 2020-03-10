75ddeb90412567a21b6628e48dd642e514bfadad
Ivan Lopez <chocosogamer@gmail.com>
2020-03-10 12:52:05 -0600

    Cambios en el Login y Registro
    Aqui se crearon el login y el registro. Se creo un evento.

--- app/src/main/java/com/runnatica/runnatica/Register.java
+++ app/src/main/java/com/runnatica/runnatica/Register.java
@@ -2,12 +2,31 @@ package com.runnatica.runnatica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+    Button Entrar, Registro;
+    EditText Usuariotxt, Contrasenatxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Entrar = (Button) findViewById(R.id.btnEntrar);
+        Registro= (Button) findViewById(R.id.btnRegistrarse);
+        Usuariotxt= (EditText) findViewById(R.id.etUsuario);
+        Contrasenatxt= (EditText) findViewById(R.id.etContraseña);
+
+        Entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
+
+
 }
