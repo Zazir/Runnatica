bfd47b90f839115eb97abae0fea0108fe60f51a4
Ivan Lopez <chocosogamer@gmail.com>
2020-03-19 00:28:05 -0600

    Login 6
    Agregue en la parte del registro del usuario la ciudad, estado y pais de residencia.

--- app/src/main/java/com/runnatica/runnatica/registro_usuario.java
+++ app/src/main/java/com/runnatica/runnatica/registro_usuario.java
@@ -12,12 +12,13 @@ import android.widget.TextView;
 public class registro_usuario extends AppCompatActivity {
 
     Button Hombre, Mujer, Foto, Registrarse;
-    EditText Nombre, Correo, Contrasena, contrasena2;
+    EditText Nombre, Correo, Contrasena, contrasena2, Ciudad, Estado, Pais;
     CheckBox Terminos;
     TextView Condiciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
         Hombre = (Button)findViewById(R.id.btnHombre);
@@ -29,6 +30,9 @@ public class registro_usuario extends AppCompatActivity {
         Contrasena = (EditText)findViewById(R.id.etContrasena);
         contrasena2 = (EditText)findViewById(R.id.etContrasenaseguridad);
         Condiciones = (TextView)findViewById(R.id.tvCondiciones);
+        Ciudad = (EditText)findViewById(R.id.etCiudad);
+        Estado = (EditText)findViewById(R.id.etEstado);
+        Pais = (EditText)findViewById(R.id.etPais);
 
 
         Condiciones.setOnClickListener(new View.OnClickListener(){
