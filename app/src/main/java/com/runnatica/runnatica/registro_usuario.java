aa34b67e06b0b75c7eb9334435cf04cf07ede3ec
Fernando Gómez <fernandozazir@gmail.com>
2020-03-19 17:12:49 -0600

    Endpoints de login y registro terminados
    No agrega aún a la base de datos, es un pedo del webService

--- app/src/main/java/com/runnatica/runnatica/registro_usuario.java
+++ app/src/main/java/com/runnatica/runnatica/registro_usuario.java
@@ -8,13 +8,26 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class registro_usuario extends AppCompatActivity {
 
     Button Hombre, Mujer, Foto, Registrarse;
-    EditText Nombre, Correo, Contrasena, contrasena2;
+    EditText Nombre, Correo, Contrasena, contrasena2, Telefono;
     CheckBox Terminos;
     TextView Condiciones;
+    private String flagTerminos = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +36,23 @@ public class registro_usuario extends AppCompatActivity {
         Hombre = (Button)findViewById(R.id.btnHombre);
         Mujer = (Button)findViewById(R.id.btnMujer);
         Foto = (Button)findViewById(R.id.btnRegistrarse);
+        Registrarse = (Button)findViewById(R.id.btnRegistrarse);
+        Terminos = (CheckBox)findViewById(R.id.checkTerminos);
         Nombre = (EditText)findViewById(R.id.etNombre);
         Correo = (EditText)findViewById(R.id.etCorreo);
-        Terminos = (CheckBox)findViewById(R.id.checkTerminos);
         Contrasena = (EditText)findViewById(R.id.etContrasena);
         contrasena2 = (EditText)findViewById(R.id.etContrasenaseguridad);
         Condiciones = (TextView)findViewById(R.id.tvCondiciones);
 
+        Registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Terminos.equals(1)){
+                    flagTerminos = "1";
+                }
+                ejecutarServicio("http://192.168.137.1:811/login/agregarUsuario.php");
+            }
+        });
 
         Condiciones.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -38,6 +61,42 @@ public class registro_usuario extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    /*
+    * Función diseñada para mandar
+    * */
+    private void ejecutarServicio(String URL) {
+        //Declaramos un StringRequest definiendo el método que utilizamos, en este caso es POST
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "OPERACION EXITOSA", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Hubo un error"+error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String,String>();
+                //Mapeo de los valores del usuario para mandarse a través de Volley
+                parametros.put("NombreYApellido", Nombre.getText().toString());
+                parametros.put("Email", Correo.getText().toString());
+                parametros.put("Contrasena", Contrasena.getText().toString());
+                parametros.put("Sexo", "hombre");
+                parametros.put("FechaNacimiento", "01");
+                parametros.put("FotoUrl", "");
+                parametros.put("Telefono", "10");
+                parametros.put("Terminos", flagTerminos);
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 }
