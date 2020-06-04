de3976a0699ea6ff44cf1691750e0b39da662240
Ivan Lopez <chocosogamer@gmail.com>
2020-06-04 13:50:36 -0500

    vistas Alex
    vistas hechas por mi

--- app/src/main/java/com/runnatica/runnatica/posponer_competencia.java
+++ app/src/main/java/com/runnatica/runnatica/posponer_competencia.java
@@ -3,11 +3,11 @@ package com.runnatica.runnatica;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CentrodeAyuda extends AppCompatActivity {
+public class posponer_competencia extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_centrode_ayuda);
+        setContentView(R.layout.activity_posponer_competencia);
     }
 }
