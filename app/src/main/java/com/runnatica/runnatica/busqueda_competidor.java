709f7c328f8ab2fb3d20b684029c9ef2f53918fe
Fernando Gómez <fernandozazir@gmail.com>
2020-06-27 13:38:11 -0500

    Buscar competencias

--- app/src/main/java/com/runnatica/runnatica/busqueda_competidor.java
+++ app/src/main/java/com/runnatica/runnatica/busqueda_competidor.java
@@ -1,20 +1,78 @@
 package com.runnatica.runnatica;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.runnatica.runnatica.adapter.MyAdapter;
+import com.runnatica.runnatica.poho.Competencias;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class busqueda_competidor extends AppCompatActivity {
 
+    private Button btnfecha, btnKm, btnEstado, btnPais;
+    private Spinner spPais, spEstado;
+    private EditText txtKM;
+    private RecyclerView rvFiltro;
+    private TextView LaFecha;
     BottomNavigationView MenuUsuario;
+
+    private String estado, pais;
+
+    private List<Competencias> listCompetencias;
+    private MyAdapter adapter;
+
+    private int[] id;
+    private String fecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda_competidor);
+
+        btnfecha = (Button)findViewById(R.id.btnFiltroFecha);
+        btnKm = (Button)findViewById(R.id.btnBuscarKm);
+        btnEstado = (Button)findViewById(R.id.btnBuscarEstado);
+        btnPais = (Button)findViewById(R.id.btnBuscarPais);
+        spPais = (Spinner) findViewById(R.id.spFiltroPais);
+        spEstado = (Spinner)findViewById(R.id.spFiltroEstado);
+        txtKM = (EditText) findViewById(R.id.etBuscarKilometros);
+        LaFecha = (TextView)findViewById(R.id.xx);
+        rvFiltro = (RecyclerView) findViewById(R.id.rvFiltroCompetencias);
+        rvFiltro.setHasFixedSize(true);
+        rvFiltro.setLayoutManager(new LinearLayoutManager(this));
+
+        listCompetencias = new ArrayList<>();
+
+        cargarSpinnerEstado();
+        cargarSpinnerPais();
 
         MenuUsuario=(BottomNavigationView)findViewById(R.id.bottomNavigation);
 
@@ -38,6 +96,56 @@ public class busqueda_competidor extends AppCompatActivity {
                 return true;
             }
         });
+
+        btnKm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int kilometros = Integer.parseInt(txtKM.getText().toString());
+                    CargarCompetencias("https://runnatica.000webhostapp.com/WebServiceRunnatica/filtroCompetencias.php?kilometros=" + kilometros);
+                }catch (NumberFormatException e){
+                    txtKM.setError("Este dato tiene que ser un número");
+                }
+
+            }
+        });
+
+        btnfecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int dia = calendar.get(Calendar.DAY_OF_MONTH);
+                int mes = calendar.get(Calendar.MONTH);
+                int ano = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog picker = new DatePickerDialog(busqueda_competidor.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        fecha = year + "-" + (month+1) + "-" + dayOfMonth;
+                        LaFecha.setText(fecha);
+                        CargarCompetencias("https://runnatica.000webhostapp.com/WebServiceRunnatica/filtroCompetencias.php?fecha=" + fecha);
+
+                    }
+                }, ano, mes, dia);
+
+                picker.show();
+
+            }
+        });
+
+        btnEstado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CargarCompetencias("https://runnatica.000webhostapp.com/WebServiceRunnatica/filtroCompetencias.php?estado=" + estado);
+            }
+        });
+
+        btnPais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CargarCompetencias("https://runnatica.000webhostapp.com/WebServiceRunnatica/filtroCompetencias.php?pais=" + pais);
+            }
+        });
     }
 
     private void home(){
@@ -55,6 +163,99 @@ public class busqueda_competidor extends AppCompatActivity {
     private void Ajustes(){
         Intent next = new Intent(this, ajustes_competidor.class);
         startActivity(next);
+    }
+
+    private void cargarSpinnerEstado() {
+        ArrayAdapter<CharSequence> opcionesSpCat = ArrayAdapter.createFromResource(this, R.array.estados, android.R.layout.simple_spinner_item);
+        spEstado.setAdapter(opcionesSpCat);
+
+        spEstado.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                estado = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void cargarSpinnerPais() {
+        ArrayAdapter<CharSequence> opcionesSpCat = ArrayAdapter.createFromResource(this, R.array.paises, android.R.layout.simple_spinner_item);
+        spPais.setAdapter(opcionesSpCat);
+
+        spPais.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                pais = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void CargarCompetencias(String URL) {
+        if (listCompetencias != null)
+            listCompetencias.clear();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //Hacer el string a json array object
+                            JSONArray array = new JSONArray(response);
+
+                            //Recorremos con un for lo que tiene el array
+                            for (int i = 0; i < array.length(); i++) {
+                                //Obtenemos los objetos tipo competencias del array
+                                JSONObject competencia = array.getJSONObject(i);
+                                id = new int[array.length()];
+
+                                //Añadir valores a los correspondientes textview
+                                listCompetencias.add(new Competencias(
+                                        competencia.getInt("id_competencia"),
+                                        competencia.getString("nom_comp"),
+                                        competencia.getString("descripcion"),
+                                        competencia.getString("precio"),
+                                        competencia.getString("foto")
+                                ));
+                                id[i] = listCompetencias.get(i).getId();
+                            }
+
+                            //Creamos instancia del adapter
+                            adapter = new MyAdapter(busqueda_competidor.this, listCompetencias, new MyAdapter.OnItemClickListener() {
+                                @Override
+                                public void OnItemClick(int position) {
+                                    //Log.i("Position", "el valor de la posición es: "+ competenciasList.get(position).getId());
+                                    String idS = new String("" + listCompetencias.get(position).getId());
+                                    launchCompetenciaView(idS);
+                                }
+                            });
+                            rvFiltro.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error de conexión con el servidor", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    private void launchCompetenciaView(String id) {
+        Intent intent = new Intent(busqueda_competidor.this, carrera_vista1.class);
+        intent.putExtra("id", id);
+        startActivity(intent);
     }
 }
 
