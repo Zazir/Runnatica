0cb14287e7f541bf00cb4f90d3c35e6650c9cafe
Ivan Lopez <chocosogamer@gmail.com>
2020-10-12 19:25:25 -0500

    Cambios Lunes

--- app/src/main/java/com/runnatica/runnatica/EditarCategorias2.java
+++ app/src/main/java/com/runnatica/runnatica/EditarCategorias2.java
@@ -1,34 +1,40 @@
 package com.runnatica.runnatica;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class EditarCategorias2 extends AppCompatActivity {
 
     private EditText NombreCategoria, CompetidoresCategorias, ForaneosCategorias, MinimoEdad, MaximoEdad;
     private Button Finalizar;
-
+    BottomNavigationView MenuOrganizador;
     private String dominio, id_categoria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_categorias2);
 
-        getString(R.string.ip);
+        dominio = getString(R.string.ip);
         getLastViewData();
 
         NombreCategoria = (EditText)findViewById(R.id.etNombreInscripcion);
@@ -36,18 +42,40 @@ public class EditarCategorias2 extends AppCompatActivity {
         ForaneosCategorias = (EditText)findViewById(R.id.etCantidadForaneos);
         MinimoEdad = (EditText)findViewById(R.id.etDesdeAnos);
         MaximoEdad = (EditText)findViewById(R.id.etHastaAnos);
-        Finalizar = (Button) findViewById(R.id.btnFinalizarCompetencia);
+        Finalizar = (Button) findViewById(R.id.btnFinalizarCompetencia2);
+        MenuOrganizador= (BottomNavigationView)findViewById(R.id.MenuOrganizador);
+
+        Menu menu = MenuOrganizador.getMenu();
+        MenuItem menuItem= menu.getItem(0);
+        menuItem.setChecked(true);
+
+        MenuOrganizador.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.menu_home) {
+                    homeOrganizador();
+                }
+                if (menuItem.getItemId() == R.id.menu_historial) {
+                    historialOrganizador();
+                }
+                if (menuItem.getItemId() == R.id.menu_regresar) {
+                    home();
+                }
+                return true;
+            }
+        });
 
         Finalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (validaciones()) {
+                Toast.makeText(getApplicationContext(), "Categoria Editada Exitosamente", Toast.LENGTH_SHORT).show();
                     peticion(dominio + "actualizarCategoria.php?edad_max="+MaximoEdad.getText().toString()+"&id_categoria="+id_categoria);
                     peticion(dominio + "actualizarCategoria.php?edad_min="+MinimoEdad.getText().toString()+"&id_categoria="+id_categoria);
                     peticion(dominio + "actualizarCategoria.php?cantidad_usuarios"+CompetidoresCategorias.getText().toString()+"&id_categoria="+id_categoria);
                     peticion(dominio + "actualizarCategoria.php?cantidad_foraneos="+ForaneosCategorias.getText().toString()+"&id_categoria="+id_categoria);
                     peticion(dominio + "actualizarCategoria.php?nombre_categoria="+NombreCategoria.getText().toString().replaceAll(" ", "%20")+"&id_categoria="+id_categoria);
-                }
+                    Atras();
             }
         });
     }
@@ -58,11 +86,14 @@ public class EditarCategorias2 extends AppCompatActivity {
     }
 
     private void peticion(String URL) {
+
         StringRequest request = new StringRequest(Request.Method.GET, URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
 
+
+                        Log.i("Mensaje",response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -78,5 +109,21 @@ public class EditarCategorias2 extends AppCompatActivity {
     private boolean validaciones() {
 
         return true;
+    }
+    private void homeOrganizador() {
+        Intent next = new Intent(this, home_organizador.class);
+        startActivity(next);
+    }
+    private void historialOrganizador(){
+        Intent next = new Intent(this, historial_organizador.class);
+        startActivity(next);
+    }
+    private void home(){
+        Intent next = new Intent(this, home.class);
+        startActivity(next);
+    }
+    private void Atras(){
+        Intent next = new Intent(this, EditarCategoria1.class);
+        startActivity(next);
     }
 }
