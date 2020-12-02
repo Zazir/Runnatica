883aa126765830337259b5ebe9242ed02b3acf8d
Alex <chocosogamer@gmail.com>
2020-12-02 09:06:42 -0600

    antes de

---
+++ app/src/main/java/com/runnatica/runnatica/InfoResultados.java
@@ -0,0 +1,14 @@
+package com.runnatica.runnatica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoResultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_resultados);
+    }
+}
